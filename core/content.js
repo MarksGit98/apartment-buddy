cede5d5d32dc56f6d958db86d6aa6da6daa55844
Khandaker Shayel <kshayel1717@bths.edu>
2022-01-29 20:08:44 -0500

    formatted streets

--- core/content.js
+++ core/content.js
@@ -1,26 +1,57 @@
 console.log("Chrome Extension Start");
 
 const updateListings = () => {
-  let cards = document.getElementsByClassName("list-card-info");
-  for (card of cards) {
-    console.log(card);
-    const address = card.getElementsByTagName("a")[0].textContent;
-    getListingData(address);
-    card.getElementsByClassName("list-card-footer")[0].innerHTML = "Zarbail";
-  }
+    let cards = document.getElementsByClassName("list-card-info");
+    for (card of cards) {
+        //console.log(card);
+        if (card.getElementsByTagName("a")[0] !== undefined) {
+            const address = card.getElementsByTagName("a")[0].textContent;
+            //console.log(address)
+            getListingData(address);
+            card.getElementsByClassName("list-card-footer")[0].innerHTML = "Zarbail";
+        }
+    }
 };
+
+//format all streets to be the same
+const cleanStreet = (street) => {
+    street = street.toUpperCase();
+    cleanUpTerms = ["APT", "#", "FLOOR", "PENTHOUSE", "TOWNHOUSE"];
+    for (term of cleanUpTerms) {
+        if (street.includes(term)) {
+            street = street.substring(0, street.indexOf(term))
+        }
+    }
+    return street
+}
+
 const getListingData = (address) => {
-  const addressBreakdown = address.split(",");
-  let [number, ...street] = addressBreakdown[0].split(" ");
-  let borough = addressBreakdown[1].trim();
-  let stateAndZip = addressBreakdown[2].trim();
+    //remove building name
+    if (address.includes("|")) {
+        address = address.substring(address.indexOf('|') + 2);
+    }
+    //console.log(address)
 
-  street = street.join(" ").replace(" ", "%20");
-  console.log(street);
-  const url = `https://whoownswhat.justfix.nyc/en/address/${borough}/${number}/${street}/summary`;
-  console.log(url);
+    const addressBreakdown = address.split(",");
+    console.log('addressBreakdown', addressBreakdown);
+    let [number, ...street] = addressBreakdown[0].split(" ");
+    console.log("number:", number)
+
+    street = street.join(" ").replace(" ", "%20");
+    street = cleanStreet(street)
+
+    console.log("street:", street)
+    let borough = addressBreakdown[1].trim();
+    console.log("borough:", borough)
+    let stateAndZip = addressBreakdown[2].trim();
+    console.log('stateZip:', stateAndZip);
+
+
+    //console.log(street);
 };
 
-const getViolationsData = (listings) => {};
+const getViolationsData = (listings) => { };
 
 updateListings();
+
+//timer = setInterval(updateListings, 250);
