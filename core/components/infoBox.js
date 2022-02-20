1b307f2ff767377f22a9181c9866671f331bab40
MarksGit98 <markbekker1998@gmail.com>
2022-02-19 20:41:06 -0500

    update

--- core/components/infoBox.js
+++ core/components/infoBox.js
@@ -1,11 +1,32 @@
-const generateInfoBox = ({ total, listings }) => {
+const generateInfoBox = (data) => {
   const infoBox = document.createElement("div");
-  infoBox.addEventListener("click", addDropDownList(listings));
+  infoBox.classList.add("infobox");
+  const totalOpenViolationsTag = document.createElement("p");
+  const avgVioPerUnitTag = document.createElement("p");
+
+  totalOpenViolationsTag.classList.add("infobox-line");
+  avgVioPerUnitTag.classList.add("infobox-line");
+
+  let description = data
+    ? `Total Number of Open Violations: ${data.total}`
+    : "No Registration Data Available";
+  totalOpenViolationsTag.innerHTML = description;
+
+  if (data && data.total && data.totalUnits) {
+    const averageUnits = `Average Violations per unit: ${parseFloat(
+      data.total / data.totalUnits
+    ).toFixed(1)}`;
+    avgVioPerUnitTag.innerHTML = averageUnits;
+  }
+
+  infoBox.appendChild(totalOpenViolationsTag);
+  infoBox.appendChild(avgVioPerUnitTag);
+  // infoBox.addEventListener("click", addDropDownList(listings));
 
   function addDropDownList(listings) {
     for (let listing in listings) {
     }
   }
-
+  console.log(infoBox);
   return infoBox;
 };
--- core/content.js
+++ core/content.js
@@ -1,25 +1,32 @@
 console.log("Chrome Extension Start");
 
 const updateListings = async () => {
+  // console.log("running");
+  // console.log(window.location.href);
+  // if (window.location.href.includes("homedetails")) {
+  //   console.log("check");
+  //   let homeDetails = document.getElementsByClassName(
+  //     "ds-home-details-chip"
+  //   )[0];
+  //   const address = homeDetails.getElementByID(
+  //     "ds-chip-property-address"
+  //   ).textContent;
+  //   console.log("address", address);
+  //   let data = await getListingData(address, "open violations");
+  //   const infoBox = generateInfoBox(data);
+  //   homeDetails.appendChild(infoBox);
+  // }
   let cards = document.getElementsByClassName("list-card-info");
   for (let card of cards) {
-    const footer = card.getElementsByClassName("list-card-footer")[0].innerHTML;
-    if (
-      !(footer.includes("Violations") || footer.includes("Registration Data"))
-    ) {
+    const footer = card.getElementsByClassName("list-card-footer")[0];
+    let infoBoxArray = card.getElementsByClassName("infobox");
+    if (infoBoxArray.length === 0) {
       if (card.getElementsByTagName("a")[0] !== undefined) {
         const address = card.getElementsByTagName("a")[0].textContent;
-
         let data = await getListingData(address, "open violations");
-        console.log(address, data);
-        let description = data
-          ? `Total Number of Open Violations: ${data.total}`
-          : "No Registration Data Available";
-        card.getElementsByClassName("list-card-footer")[0].innerHTML =
-          description;
-        card.getElementsByClassName("list-card-footer")[0].style[
-          "background-color"
-        ] = "#FF00FF";
+        infoBox = generateInfoBox(data);
+        footer.style["height"] = "60px";
+        footer.appendChild(infoBox);
       }
     }
   }
@@ -41,10 +48,17 @@ const getListingData = async (address, type) => {
   let state = stateAndZip[0].toUpperCase();
   //if (state !== 'NY') return 'You must be in NYC'
   if (type === "open violations") {
-    data = await fetchAddressData({ number, street, borough, state });
+    data = await fetchViolationData({ number, street, borough, state });
+    const units = await fetchUnitData({ number, street, borough, state });
     if (data.length > 0) {
-      return getOpenViolations(data);
-    } else return undefined;
+      return getOpenViolations(data, units);
+    } else {
+      const altStreet = await getFormattedStreet(number + " " + street);
+      data = await fetchViolationData({ number, altStreet, borough, state });
+      if (data.length > 0) {
+        return getOpenViolations(data, units);
+      } else return undefined;
+    }
   }
 };
 
